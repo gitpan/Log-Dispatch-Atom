933f7d0d3b2ddaa166ec5671570aeebc3ae162e7
Dominic Mitchell <cpan@happygiraffe.net>
2005-12-08 13:25:32 +0000

    Import of HDM/Log-Dispatch-Atom-0.03 from CPAN.
    gitpan-cpan-distribution: Log-Dispatch-Atom
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         HDM/Log-Dispatch-Atom-0.03.tar.gz
    gitpan-cpan-author:       HDM
    gitpan-cpan-maturity:     released

--- lib/Log/Dispatch/Atom.pm
+++ lib/Log/Dispatch/Atom.pm
@@ -1,8 +1,8 @@
-# @(#) $Id: Atom.pm 1081 2005-11-14 12:59:41Z dom $
+# @(#) $Id: Atom.pm 1102 2005-12-07 14:33:19Z dom $
 
 package Log::Dispatch::Atom;
 
-$VERSION = 0.02;
+$VERSION = 0.03;
 
 use warnings;
 use strict;
@@ -10,10 +10,12 @@ use strict;
 use Carp qw( carp croak );
 use Fcntl qw( :flock );
 use POSIX qw( strftime );
-use Params::Validate;
+use Params::Validate qw( validate SCALAR HASHREF );
+use Sys::Hostname qw( hostname );
 use XML::Atom 0.15;    # We need add_entry(mode=>insert).
 use XML::Atom::Entry;
 use XML::Atom::Feed;
+use XML::Atom::Person;
 
 use base qw( Log::Dispatch::Output );
 
@@ -26,37 +28,65 @@ sub new {
     my %p     = @_;
 
     my $self = bless {}, $class;
+    # Log::Dispatch setup.
     $self->_basic_init( %p );
+    # Our setup.
     $self->_init( %p );
     return $self;
 }
 
 sub _init {
     my $self = shift;
-    my %p = validate( @_, { file => 1, feed_id => 0, feed_title => 0 } );
-    $self->{ file } = $p{ file };
-    $self->{ feed_id } = $p{ feed_id } if $p{ feed_id };
-    $self->{ feed_title } = $p{ feed_title } if $p{feed_title};
+    my %p    = validate(
+        @_,
+        {
+            file        => { type => SCALAR },
+            feed_id     => { type => SCALAR, optional => 1 },
+            feed_title  => { type => SCALAR, optional => 1 },
+            feed_author => { type => HASHREF, optional => 1 },
+        }
+    );
+    $self->{ file }        = $p{ file };
+    $self->{ feed_id }     = $p{ feed_id } if $p{ feed_id };
+    $self->{ feed_title }  = $p{ feed_title } if $p{ feed_title };
+    $self->{ feed_author } = $p{ feed_author } if $p{ feed_author };
     return;
 }
 
-sub _now { strftime "%Y-%m-%dT%H:%M:%SZ", gmtime }
+sub _now_datetime { strftime "%Y-%m-%dT%H:%M:%SZ", gmtime }
+sub _now_date     { strftime "%Y-%m-%d", gmtime }
 
 sub log_message {
     my $self = shift;
-    my %p = @_;
+    my %p    = validate(
+        @_,
+        {
+            id     => { type => SCALAR,  default  => $self->_default_id },
+            author => { type => HASHREF, optional => 1 },
+        }
+    );
     my $fh = eval {
-        my $fh    = $self->_lock_and_open();
-        my $feed  = $self->_get_feed_from_handle( $fh );
+        my $fh   = $self->_lock_and_open();
+        my $feed = $self->_get_feed_from_handle( $fh );
         $self->_new_entry( $feed, \%p );
         $self->_write_feed( $fh, $feed );
         return $fh;
     };
+
     # Take care to avoid clobbering $@.
     my $err = $@;
     eval { $self->_unlock_and_close( $fh ) };
     die $err if $err;
     return;
+}
+
+{
+    my $i;
+    sub _default_id {
+        my $self = shift;
+        return sprintf( "tag:%s,%s:%d/%d/%d",
+            hostname(), $self->_now_date(), time(), $$, ++$i );
+    }
 }
 
 sub _get_feed_from_handle {
@@ -68,12 +98,29 @@ sub _get_feed_from_handle {
         return XML::Atom::Feed->new( $fh );
     }
     else {
-        # Create a new feed.
-        my $feed = XML::Atom::Feed->new( Version => '1.0' );
-        $feed->id( $self->{ feed_id } ) if $self->{ feed_id };
-        $feed->title( $self->{ feed_title } ) if $self->{ feed_title };
-        return $feed;
+        return $self->_new_feed();
     }
+}
+
+sub _new_feed {
+    my $self = shift;
+    my $feed = XML::Atom::Feed->new( Version => '1.0' );
+    $feed->id( $self->{ feed_id } )       if $self->{ feed_id };
+    $feed->title( $self->{ feed_title } ) if $self->{ feed_title };
+    if ( my $author = $self->{ feed_author } ) {
+        $feed->author( $self->_new_person( $author ) );
+    }
+    return $feed;
+}
+
+sub _new_person {
+    my $self     = shift;
+    my ( $args ) = @_;
+    my $person   = XML::Atom::Person->new( Version => '1.0' );
+    $person->name( $args->{ name } )   if $args->{ name };
+    $person->email( $args->{ email } ) if $args->{ email };
+    $person->uri( $args->{ uri } )     if $args->{ uri };
+    return $person;
 }
 
 sub _new_entry {
@@ -82,10 +129,15 @@ sub _new_entry {
     my $entry = XML::Atom::Entry->new( Version => '1.0' );
     $entry->title( "$args->{message}" );
     $entry->content( "$args->{message}" );
+    $entry->id( "$args->{id}" );
 
-    my $now = _now();
+    my $now = _now_datetime();
     $feed->updated( $now );
     $entry->updated( $now );
+
+    if ( $args->{ author } ) {
+        $entry->author( $self->_new_person( $args->{ author } ) );
+    }
 
     $feed->add_entry( $entry, { mode => 'insert' } );
     return $entry;
@@ -208,9 +260,25 @@ If not specified, it will be omitted, which is in violation of the
 Atom specification.  For more information, see
 L<http://www.atomenabled.org/developers/syndication/#requiredFeedElements>.
 
-B<XXX> This should probably just use the I<name> parameter.
+B<XXX> This should probably just use the I<name> parameter.  What do you
+think?  Let me know.
+
+=item I<feed_author> [optional]
+
+The author details of a feed.  This is specified as a hash reference,
+which must contain one or more of the three keys I<name>, I<email> and
+I<uri>.
+
+In order to create a valid Atom feed, you must either supply an author
+in every single entry (log message), or ensure that the feed itself has
+an author.  The latter is probably the easier solution, so I recommend
+this parameter be supplied.
 
 =back
+
+B<NB>:  The I<feed_*> parameters will only be used when a new feed is
+being created.  If you are creating a new object for an existing feed,
+they will be ignored.
 
 =item log_message()
 
@@ -226,6 +294,23 @@ The actual log message.
 =item I<level> [mandatory]
 
 The level of the message.  See L<Log::Dispatch> for a full list.
+
+=item I<id> [optional]
+
+Each entry requires an id in order for the feed as a whole to be a valid
+Atom document.  It's used by readers of Atom documents to determine
+whether or not an entry has been seen previously.
+
+If not specified, this will default to an URL comprising the current
+time plus the pid plus the hostname plus a monotonically increasing
+integer.  eg: tag:fred.example.com,2005-12-07:1133946771/20827/2.  This
+should be good enough for a uniqueness test.
+
+=item I<author> [optional]
+
+You can specify author details for an individual entry if desired.  The
+author parameter is expected to be a hash reference, which must contain
+one or more of the keys I<name>, I<email> or I<uri>.
 
 =back
 
--- t/Log-Dispatch-Atom.t
+++ t/Log-Dispatch-Atom.t
@@ -1,5 +1,5 @@
 #!perl
-# @(#) $Id: Log-Dispatch-Atom.t 1033 2005-11-03 23:12:20Z dom $
+# @(#) $Id: Log-Dispatch-Atom.t 1102 2005-12-07 14:33:19Z dom $
 
 use strict;
 use warnings;
@@ -7,6 +7,7 @@ use warnings;
 use File::Temp qw( tempfile );
 use Log::Dispatch::Atom;
 use POSIX qw( strftime );
+use Sys::Hostname;
 use Test::More 'no_plan';
 use XML::Atom::Feed;
 
@@ -24,6 +25,8 @@ sub test_basics {
     isa_ok( $log, 'Log::Dispatch::Atom' );
     can_ok( $log, qw( log ) );
 
+    my $today = strftime "%Y-%m-%d", gmtime;
+    my $now = time;
     $log->log( level => 'info', message => 'hello world' );
     my $feed = eval { XML::Atom::Feed->new( $fn ) };
     is( $@, '', 'log(1) No problems parsing feed.' );
@@ -32,7 +35,10 @@ sub test_basics {
     is( $entries[0]->title, 'hello world', 'log(1) made correct title' );
     is( $entries[0]->content->body,
         'hello world', 'log(1) made correct content' );
+    my $expected_id = "tag:" . hostname() . ",$today:$now/$$/1";
+    is( $entries[0]->id, $expected_id, 'log(1) made correct id' );
 
+    $now = time;
     $log->log( level => 'info', message => 'hello world#2' );
     $feed = eval { XML::Atom::Feed->new( $fn ) };
     is( $@, '', 'log(2) No problems parsing feed.' );
@@ -44,26 +50,57 @@ sub test_basics {
         'hello world#2',
         'log(2) made correct content'
     );
+    $expected_id = "tag:" . hostname() . ",$today:$now/$$/2";
+    is( $entries[0]->id, $expected_id, 'log(2) made correct id' );
     return;
 }
 
 sub test_feed_extras {
     my $fn  = tempfilename();
     my $log = Log::Dispatch::Atom->new(
-        name       => 'test_feed_extras',
-        min_level  => 'debug',
-        file       => $fn,
-        feed_title => 'My Test Log',
-        feed_id    => 'http://example.com/log/',
+        name        => 'test_feed_extras',
+        min_level   => 'debug',
+        file        => $fn,
+        feed_title  => 'My Test Log',
+        feed_id     => 'http://example.com/log/',
+        feed_author => {
+            name  => 'Fred Flintstone',
+            email => 'fred@flintstones.com',
+            uri   => 'http://fred.flintstones.com/',
+        },
     );
     isa_ok( $log, 'Log::Dispatch::Atom' );
 
-    $log->log( level => 'info', message => 'hello world' );
+    $log->log(
+        level   => 'info',
+        message => 'hello world',
+        author  => { name => 'Barney' }
+    );
     my $feed = eval { XML::Atom::Feed->new( $fn ) };
     is( $@, '', 'test_feed_extras: No problems parsing feed.' );
     is( $feed->title, 'My Test Log', 'test_feed_extras: title' )
         or diag( slurp( $fn ) );
     is( $feed->id, 'http://example.com/log/', 'test_feed_extras: id' );
+
+    isa_ok( $feed->author, 'XML::Atom::Person', 'test_feed_extras: author' );
+    is(
+        $feed->author->name,
+        'Fred Flintstone',
+        'test_feed_extras: author/name',
+    );
+    is( $feed->author->email, 'fred@flintstones.com',
+        'test_feed_extras: author/email',
+    );
+    is(
+        $feed->author->uri,
+        'http://fred.flintstones.com/',
+        'test_feed_extras: author/uri',
+    );
+
+    my ( $e1 ) = $feed->entries;
+    isa_ok( $e1->author, 'XML::Atom::Person',
+        'test_feed_extras: entry/author' );
+    is( $e1->author->name, 'Barney', 'test_feed_extras: entry/author/name' );
     return;
 }
 
